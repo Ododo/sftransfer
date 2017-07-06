4b4b8ad38c41459a64b58fafe0ab7e6d088bcf2b
Olivier <olivierdautricourt@gmail.com>
2017-07-06 15:26:30 +0200

    removing unused import

--- fifoserv.py
+++ fifoserv.py
@@ -12,7 +12,6 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.twofactor.hotp import HOTP
 
 from queue import Queue
 from collections import OrderedDict
