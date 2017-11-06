c94f9e309693c97532a632740a2f3c59f5b7eda7
Ododo <olivierdautricourt@gmail.com>
2017-11-06 01:22:28 +0100

    update default config

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import os
 
 #tcp filetransfer default values
 TCP_CHUNK_SIZE=4096
-TCP_PORT=6667
+TCP_PORT=8888
 
 #RDV server default values
 SERVER_KEY = os.urandom(16) #must be per instance generated
 SERVER_PORT = 80
-SERVER_IP = "193.54.15.207"
+SERVER_IP = "<server_public_ip>"
 CERT_FILE="pems/cert.pem"
 KEY_FILE="pems/key.pem"
 MAX_ITEM = 1024
